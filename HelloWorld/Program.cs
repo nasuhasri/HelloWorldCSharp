c2bdd6217a39374fd11d31ec1e664d1f487952c4
Nasuha Asri <NASAS@orsted.com>
2023-11-24 22:24:05 +0800

    variables and constants

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,6 +11,24 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello my family!");
+
+            // can also use var so C# will define it automatically
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Ali";
+            bool isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            // format string - shows min and max value for byte
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
         }
     }
 }
