61c93a0b7bad4e54b455bea56795a63870e5a5cb
Nasuha Asri <NASAS@orsted.com>
2023-11-24 22:01:11 +0800

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; // basic utility classes and primitive types typewriter system
+using System.Collections.Generic; // work with list collections
+using System.Linq; // work with data
+using System.Text; // work with text, coding etc
+using System.Threading.Tasks; // used to build multithreaded application
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my family!");
+        }
+    }
+}
