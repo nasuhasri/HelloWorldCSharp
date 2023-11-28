4c1d9158c1840c1dcf58ddbb5e82c859b783be12
Nasuha Asri <nasuhasri00@gmail.com>
2023-11-28 14:44:00 +0800

    string

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -18,31 +18,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            // creating time span objects
-            var timeSpan = new TimeSpan(1, 2, 3); // hours/minutes/seconds
+            var fullname = "Mosh Hamedani ";
+            Console.WriteLine("Trim: '{0}'", fullname.Trim());
 
-            var timeSpan1 = new TimeSpan(1, 0, 0);
-            var timeSpan2 = TimeSpan.FromHours(1);
-
-            Console.WriteLine(timeSpan2); // 01:00:00
-
-            var start = DateTime.Now; // returns the current date/time
-            var end = DateTime.Now.AddMinutes(30);
-            var duration = end - start;
-
-            Console.WriteLine("Start: " + start);
-            Console.WriteLine("End: " + end);
-            Console.WriteLine("Duration: " + duration);
-
-            // Properties
-            Console.WriteLine("Minutes: " + timeSpan.Minutes); // return 2
-            // 62.05, from timespan object -> 1 hour: 60 minutes + 2 minutes + (3 sec/60)
-            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
-
-            // Add
-            // add 8 mins to current timespan object
-            // return Add Example: 01:10:03
-            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C")); // RM29.95
         }
     }
 }
