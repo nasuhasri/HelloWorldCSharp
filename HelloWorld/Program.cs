073e5f98ae77f58ee45b30b41c2d45b4764898e5
Nasuha Asri <nasuhasri00@gmail.com>
2023-11-28 22:10:58 +0800

    exercise control flow

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,32 +7,86 @@ using System.Threading.Tasks; // used to build multithreaded application
 
 namespace HelloWorld
 {
-    public enum ShippingMethod
-    {
-        RegularAirMail = 1, // if not set, will start with 0
-        RegisteredAirMail = 2,
-        Express = 3
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            var builder = new StringBuilder();
-            builder.Append('-', 10); // append 10x
-            builder.AppendLine();
-            builder.Append("Header");
-            builder.AppendLine();
-            builder.Append('-', 10);
+            // Exercise - Control Flow
 
-            var builder1 = new StringBuilder();
-            builder1.Append('-', 10) // append 10x
-                .AppendLine()
-                .Append("Header")
-                .AppendLine()
-                .Append('-', 10);
+            // Exercise 1
+            Console.Write("Enter number: ");
+            var input = Console.ReadLine();
+            var num = int.Parse(input);
 
-            Console.WriteLine(builder);
+            if (num >= 1 && num <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            // Exercise 2
+            Console.Write("Enter number 1: ");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter number 2: ");
+            var num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 >= num2)
+            {
+                Console.WriteLine(num1);
+            }
+            else
+            {
+                Console.WriteLine(num2);
+            }
+
+            // Exercise 3
+            /*
+             * the height of the image is longer than the width - portrait
+             * Images where the width is longer - landscape
+             */
+            Console.Write("Enter width: ");
+            var width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter height: ");
+            var height = Convert.ToInt32(Console.ReadLine());
+
+            if (height > width)
+            {
+                Console.WriteLine("Portrait");
+            }
+            else
+            {
+                Console.WriteLine("Landscape");
+            }
+
+            // Exercise 4
+            Console.Write("Enter speed limit: ");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter speed of car: ");
+            var speedCar = Convert.ToInt32(Console.ReadLine());
+
+            if (speedCar < speedLimit)
+            {
+                Console.WriteLine("Okay");
+            }
+            else
+            {
+                var demeritPoints = (speedCar - speedLimit) / 5;
+
+                if (demeritPoints < 12)
+                {
+                    Console.WriteLine("Demerit Points: " + demeritPoints);
+                }
+                else
+                {
+                    Console.WriteLine("License suspended!");
+                }
+            }
         }
 
     }
