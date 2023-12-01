1d5451e956a933ff4f3d3522a6756a2f067a026b
Nasuha Asri <nasuhasri00@gmail.com>
2023-12-02 01:07:58 +0800

    array and list exercise

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,54 +12,164 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int> { 1, 2 };
-            var smallest = GetSmallests(numbers, 3);
+            // Exercise - Array and Lists
 
-            foreach (var number in smallest)
-            {
-                Console.WriteLine(number);
-            }
+            // Exercise 1
+            Exercise1();
+
+            // Exercise 2
+            Exercise2();
+
+            // Exercise 3
+            Exercise3();
+
+            // Exercise 4
+            Exercise4();
+
+            // Exercise 5
+            Exercise5();
+            
         }
 
-        public static List<int> GetSmallests(List<int> list, int count)
+        public static void Exercise1()
         {
-            // defensive programming
-            if (list == null)
+            var names = new List<string>();
+
+            while (true)
             {
-                throw new ArgumentNullException("list");
-            }
-             
-            if (count > list.Count || count <= 0)
-            {
-                throw new ArgumentOutOfRangeException("count", "Count should be between 1 and the number of the elements in the list.");
-            }
+                Console.WriteLine("Enter names: ");
+                var input = Console.ReadLine();
 
-            var buffer = new List<int>(list); // pass original list here
-            var smallests = new List<int>();
-
-            while (smallests.Count < count)
-            {
-                var min = GetSmallest(buffer);
-                smallests.Add(min);
-                buffer.Remove(min);
-            }
-
-            return smallests;
-        }
-
-        public static int GetSmallest(List<int> list)
-        {
-            var min = list[0];
-
-            for (var i = 0; i < list.Count; i++)
-            {
-                if (list[i] < min)
+                if (string.IsNullOrEmpty(input))
                 {
-                    min = list[i];
+                    break;
+                }
+
+                names.Add(input);
+            }
+
+            if (names.Count > 2)
+            {
+                Console.WriteLine("{0}, {1} and {2} others like your post.", names[0], names[1], names.Count - 2);
+            }
+            else if (names.Count == 2)
+            {
+                Console.WriteLine("{0} and {1} like your post.", names[0], names[1]);
+            }
+            else if (names.Count == 1)
+            {
+                Console.WriteLine("{0} like your post.", names[0]);
+            }
+        }
+
+        public static void Exercise2()
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+
+            var array = new char[name.Length];
+
+            Console.WriteLine("name length: " + name.Length);
+
+            for (int i = name.Length; i > 0; i--)
+            {
+                // index start with 2
+                array[name.Length - i] = name[i - 1];
+            }
+
+            var reversed = new string(array);
+            Console.WriteLine("Reversed name: " + reversed);
+        }
+
+        public static void Exercise3()
+        {
+            Console.WriteLine("Enter 5 numbers: ");
+            var numList = new List<int>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                var intInput = Convert.ToInt32(input);
+
+                if (!numList.Contains(intInput))
+                {
+                    numList.Add(Convert.ToInt32(input));
+                }
+                else
+                {
+                    Console.WriteLine("Number has been previously entered. Please re-try!");
+                }
+
+                if (numList.Count == 5)
+                {
+                    break;
                 }
             }
 
-            return min;
+            numList.Sort();
+
+            for (var i = 0; i < numList.Count; i++)
+            {
+                Console.WriteLine(numList[i]);
+            }
+        }
+
+        public static void Exercise4()
+        {
+            Console.Write("Enter a number or type 'Quit': ");
+            var input = Console.ReadLine();
+
+            var numList = new List<int>();
+
+            while (input != "Quit")
+            {
+                var num = Convert.ToInt32(input);
+
+                if (!numList.Contains(num))
+                {
+                    numList.Add(num);
+                }
+
+                Console.Write("Enter a number or type 'Quit': ");
+                input = Console.ReadLine();
+            }
+
+            numList.Sort();
+
+            for (var i = 0; i < numList.Count; i++)
+            {
+                Console.WriteLine(numList[i]);
+            }
+        }
+
+        public static void Exercise5()
+        {
+            string[] elements;
+            while (true)
+            {
+                Console.WriteLine("Enter a list of comma separated numbers: ");
+                var input = Console.ReadLine();
+
+                elements = input.Split(',');
+
+                if (elements.Length >= 5)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Invalid list!");
+            }
+
+            var numList = new List<int>();
+
+            foreach (var item in elements)
+            {
+                numList.Add(Convert.ToInt32(item));
+            }
+
+            numList.Sort();
+
+            Console.WriteLine("The three smallest numbers are: {0}, {1}, {2}", numList[0], numList[1], numList[2]);
         }
     }
 }
